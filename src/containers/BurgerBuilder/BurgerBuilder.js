a3c517ecff3741c60d35dcc2f7c6e8abc637c84a
emilyjspencer <emilyspencer89@hotmail.co.uk>
2020-07-22 16:56:49 +0100

    If the user tries to delete a particular ingredient when no ingredient has been added - nothing happens- no React error

--- src/containers/BurgerBuilder/BurgerBuilder.js
+++ src/containers/BurgerBuilder/BurgerBuilder.js
@@ -57,6 +57,9 @@ class BurgerBuilder extends Component {
 
   removeIngredientsHandler = (type) => {
     const oldCount = this.state.ingredients[type];
+    if (oldCount <= 0) {
+      return;
+    }
     const updatedCount = oldCount - 1;
     const updatedIngredients = {
       ...this.state.ingredients
