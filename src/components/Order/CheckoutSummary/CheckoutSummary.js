f25b4cfb8e947caf32a84c24d8c0f4424d225426
emilyjspencer <emilyspencer89@hotmail.co.uk>
2020-08-05 13:28:54 +0100

    Uses the history prop's goBack method to go back to the last page upon click of the cancel button. Uses the history prop's replace() method to go to the currently non-existent contact-data page upon click of the continue button

--- src/components/Order/CheckoutSummary/CheckoutSummary.js
+++ src/components/Order/CheckoutSummary/CheckoutSummary.js
@@ -14,11 +14,13 @@ const checkoutSummary = (props) => {
             <Burger ingredients={props.ingredients}/>
           </div>
           <Button 
-            
-            btnType="Danger">CANCEL</Button>
+            btnType="Danger"
+            clicked={props.checkoutCancelled}
+            >CANCEL</Button>
           <Button 
-            
-            btnType="Success">CONTINUE</Button>
+            btnType="Success"
+            clicked={props.checkoutContinued}
+            >CONTINUE</Button>
       </div>
   )
 
